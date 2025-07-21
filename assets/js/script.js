ed2e3e6f71a4997c1add3e607de337e582fa29fa
sarajelic <2zcc1qnq@students.codeinstitute.net>
2025-07-21 22:59:43 +0000

    Add favicon, add final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,21 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     runGame("addition");
 });
+document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+});
+
 /**
  * The main game "loop" called when the script is first loaded
  * and after the user's answer has been processed. 
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
